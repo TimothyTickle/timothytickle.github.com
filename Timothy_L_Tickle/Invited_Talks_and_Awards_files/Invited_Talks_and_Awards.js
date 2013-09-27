d81fe87d18a245d30a63f333c3f97ff8ee216c29
timothyltickle <timothyltickle@localhost>
2013-09-27 01:00:09 -0400

    Adding in first draft website

---
+++ Timothy_L_Tickle/Invited_Talks_and_Awards_files/Invited_Talks_and_Awards.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20130927
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Invited_Talks_and_Awards_files/Invited_Talks_and_AwardsMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
