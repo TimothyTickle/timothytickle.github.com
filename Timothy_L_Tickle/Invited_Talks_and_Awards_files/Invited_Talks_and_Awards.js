b71973f97c37d02d0d8fe883bf389a58f83ce841
timothyltickle <timothyltickle@localhost>
2013-11-11 20:15:25 -0500

    Switched to a new template.

--- Timothy_L_Tickle/Invited_Talks_and_Awards_files/Invited_Talks_and_Awards.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20130929
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Invited_Talks_and_Awards_files/Invited_Talks_and_AwardsMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
